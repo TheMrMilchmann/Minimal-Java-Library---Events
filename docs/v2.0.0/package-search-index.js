44449bc54b2b2d484b46803024af5dcecae835fb
Leon Linhart <themrmilchmann@gmail.com>
2019-03-10 23:24:06 +0100

    feat: manually add v2.0.0 release docs

---
+++ docs/v2.0.0/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.github.themrmilchmann.mjl.events"},{"l":"com.github.themrmilchmann.mjl.events.internal"},{"l":"com.github.themrmilchmann.mjl.events.util.concurrent"}]
